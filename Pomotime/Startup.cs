05f23251153ef13f82b5e2563c498a77d9c268a3
rodrigomaia17 <rodrigo.maia.pereira@gmail.com>
2014-05-22 22:29:36 -0300

    Esqueleto do projeto inicial

---
+++ Pomotime/Startup.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Owin;
+using Owin;
+using Pomotime;
+
+[assembly: OwinStartup(typeof(Startup))]
+namespace Pomotime
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // Any connection or hub wire up and configuration should go here
+            app.MapSignalR();
+        }
+    }
+}
